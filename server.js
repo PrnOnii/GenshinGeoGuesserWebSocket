f3fc34102842376d8956d3094ba294b1100c0036
Kevan Sadeghi <ksadeghi27@gmail.com>
2021-11-25 15:21:49 +0100

    Fourth Try

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const server = express()
 const wss = new Server({ server });
 let count = 0;
 
+const broadcast = (data, ws) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN && client !== ws) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', event => {
