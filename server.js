cf074450e9fed15dff10a0ccfe22c266a1f270e8
Kevan Sadeghi <ksadeghi27@gmail.com>
2021-11-27 00:34:04 +0100

    First gameplay test

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const server = express()
 
 const wss = new Server({ server });
 
-const players = [];
-const environment = {
-  hasGM: false,
+var id = 0;
+var lookup = [];
+var gameMaster = null;
+var players = [];
+var answers = [];
+var environment = {
+  gameStarted: false,
+  roundStarted: false,
 };
 
 const broadcast = (data, ws) => {
@@ -27,11 +32,14 @@ const broadcast = (data, ws) => {
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.id = ++id;
+  lookup[ws.id] = ws;
   // Initial data send
   ws.send(JSON.stringify({ 
     action: "STATUS_INIT", 
     data: {
       environment: environment,
+      gameMaster: gameMaster,
       players: players
     }
   }));
@@ -43,31 +51,104 @@ wss.on('connection', (ws) => {
     switch(data.action) {
       case "KEEP_ALIVE":
         ws.send(JSON.stringify({ action: "KEEP_ALIVE", status: "ok keeping alive" }));
-        // broadcast({ status: "Success", data: count, tz: new Date()}, ws)
         break;
       case "ADD_PLAYER":
-        players.push({ name: data.name, score: 0, isGM: data.isGM});
         if (data.isGM) {
-          environment.hasGM = true;
-          broadcast({ action: "HAS_GM", data: null }, ws);
+          gameMaster = { name: data.name, id: ws.id };
+          ws.send(JSON.stringify({ action: "HAS_GM", gameMaster: gameMaster }));
+          broadcast({ action: "HAS_GM", gameMaster: gameMaster }, ws);
+          break;
         }
+        players.push({ name: data.name, score: 0, id: ws.id, answered: false });
         ws.send(JSON.stringify({ action: "UPDATE_PLAYERS", data: players }));
         broadcast({ action: "UPDATE_PLAYERS", data: players }, ws);
         break;
+      case "REMOVE_PLAYER":
+        var player = players.find(pl => pl.id == data.playerId);
+        console.log(players.indexOf(player));
+        if (answers.length > 0) { answers.splice(players.indexOf(player), 1)}
+        players.splice(players.indexOf(player), 1);
+        ws.send(JSON.stringify({ action: "UPDATE_PLAYERS", data: players }));
+        broadcast({ action: "UPDATE_PLAYERS", data: players }, ws);
+        break;
+      case "START_GAME":
+        players.forEach(pl => {
+          answers.push({ playerId : pl.id, x: 0, y: 0, distance: 0, score: 0});
+        })
+        broadcast(JSON.stringify({ action: "ROUND_STARTED" }), ws);
+        break;
+      case "SUBMIT_ANSWER":
+        let index = answers.findIndex(a => a.playerId == data.player.id);
+        answers[index].x = data.answer.x;
+        answers[index].y = data.answer.y;
+        players.find(p => p.id == data.player.id).answered == true;
+        ws.send(JSON.stringify({
+          action: "UPDATE_PLAYERS",
+          players: players
+        }));
+        broadcast(JSON.stringify({
+          action: "UPDATE_PLAYERS",
+          players: players
+        }), ws);
+        break;
+      case "END_ROUND":
+        answers.forEach(answer => {
+          answer.distance = computeDistance(answer, data.goodAnswer);
+          answer.score = computeScore(answer.distance);
+          players[players.findIndex(p => p.id == answer.playerId)].score += answer.score;
+        })
+        ws.send(JSON.stringify({
+          action: "SHOW_RESULTS",
+          players: players,
+          answers: answers
+        }));
+        broadcast(JSON.stringify({
+          action: "SHOW_RESULTS",
+          players: players,
+          answers: answers
+        }), ws)
+        break;
+      case "NEW_ROUND":
+        answers = [];
+        players.forEach(pl => {
+          answers.push({ playerId : pl.id, x: 0, y: 0, distance: 0, score: 0});
+        })
+        broadcast(JSON.stringify({ action: "ROUND_STARTED" }), ws);
+        break;
+      case "END_GAME":
+        gameMaster = null;
+        players = [];
+        answers = [];
+        environment = {
+          gameStarted: false,
+        };
       case "RESET_VALUES":
+        gameMaster = null;
         players = [];
         environment = {
-          hasGM: false,
+          gameStarted: false,
         };
         break;
       default:
         ws.send(JSON.stringify({ status: "Error", data: "Unknown message received", tz: new Date() }));
     }
   });
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+
+    console.log('Client' + ws.id  + 'disconnected')
+  });
 });
 
+function computeDistance(answer, goodAnswer) {
+  let a = parseInt(this.goodAnswer.x) - parseInt(answerx);
+  let b = parseInt(this.goodAnswer.y) - parseInt(answery);
 
+  return Math.round(Math.sqrt(a*a + b*b));
+}
+
+function computeScore(distance) {
+  return Math.round(5000 * Math.pow(0.998, distance));
+}
 // setInterval(() => {
 //   wss.clients.forEach((client) => {
 //     client.send(new Date().toTimeString());
